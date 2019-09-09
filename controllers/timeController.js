a4013bcdb34237de783437fabd67985293332293
FABIAN ALEJANDRO VAZQUEZ CHAVEZ <fabian.vazquez@ibm.com>
2019-09-09 18:39:25 -0500

    otro post sin t_inicio

--- controllers/timeController.js
+++ controllers/timeController.js
@@ -31,6 +31,16 @@ exports.timePOST = function (req, res) {
         res.json({ rc: '00', msg: 'El registro de la prueba se realizó con éxito', result: time });
     });
 };
+// POST - CREATE
+exports.timePOST = function (req, res) {
+    const time = new Time({ t_salida: req.body.t_salida, id_cajon: req.body.id_cajon, id_usuario: req.body.id_usuario });
+    time.save(function (err, time) {
+        if (err)
+            res.json({ rc: '99', msg: 'Ocurrió un error al intentar registrar la prueba' });
+
+        res.json({ rc: '00', msg: 'El registro de la prueba se realizó con éxito', result: time });
+    });
+};
 
 // PUT - UPDATE
 exports.timePUT = function (req, res) {
